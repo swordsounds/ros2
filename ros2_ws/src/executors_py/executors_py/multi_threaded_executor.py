03cf5466f96987b99a96a860bf25b9ac7578f960
swordsounds <migettiny@gmail.com>
2025-05-16 12:24:10 -0230

    Components and Executors

--- ros2_ws/src/executors_py/executors_py/multi_threaded_executor.py
+++ ros2_ws/src/executors_py/executors_py/multi_threaded_executor.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup, MutuallyExclusiveCallbackGroup
 import time
 
 
 class Node1(Node):
     def __init__(self):
         super().__init__("node1")
-        self.timer1_ = self.create_timer(1.0, self.callback_timer1)
-        self.timer2_ = self.create_timer(1.0, self.callback_timer2)
-        self.timer3_ = self.create_timer(1.0, self.callback_timer3)
+        self.cb_group1 = MutuallyExclusiveCallbackGroup()
+        self.cb_group2 = ReentrantCallbackGroup()
+        self.timer1_ = self.create_timer(1.0, self.callback_timer1, callback_group=self.cb_group1)
+        self.timer2_ = self.create_timer(1.0, self.callback_timer2, callback_group=self.cb_group2)
+        self.timer3_ = self.create_timer(1.0, self.callback_timer3, callback_group=self.cb_group2)
 
     def callback_timer1(self):
         time.sleep(2.0)
@@ -23,11 +27,30 @@ class Node1(Node):
         time.sleep(2.0)
         self.get_logger().info("cb 3")
 
+class Node2(Node):
+    def __init__(self):
+        super().__init__("node2")
+        self.timer4_ = self.create_timer(1.0, self.callback_timer4)
+        self.timer5_ = self.create_timer(1.0, self.callback_timer5)
 
+
+    def callback_timer4(self):
+        time.sleep(2.0)
+        self.get_logger().info("cb 4")
+
+    def callback_timer5(self):
+        time.sleep(2.0)
+        self.get_logger().info("cb 5")
+        
 def main(args=None):
     rclpy.init(args=args)
     node1 = Node1()
-    rclpy.spin(node1)
+    # rclpy.spin(node1, MultiThreadedExecutors())
+
+    executor = MultiThreadedExecutor()
+    executor.add_node(node1)
+    executor.spin()
+
     rclpy.shutdown()
 
 
--- ros2_ws/src/executors_py/executors_py/single_threaded_executor.py
+++ ros2_ws/src/executors_py/executors_py/single_threaded_executor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.executors import SingleThreadedExecutor
 import time
 
 
@@ -23,11 +24,32 @@ class Node1(Node):
         time.sleep(2.0)
         self.get_logger().info("cb 3")
 
+class Node2(Node):
+    def __init__(self):
+        super().__init__("node2")
+        self.timer4_ = self.create_timer(1.0, self.callback_timer4)
+        self.timer5_ = self.create_timer(1.0, self.callback_timer5)
+
+
+    def callback_timer4(self):
+        time.sleep(2.0)
+        self.get_logger().info("cb 4")
+
+    def callback_timer5(self):
+        time.sleep(2.0)
+        self.get_logger().info("cb 5")
 
 def main(args=None):
     rclpy.init(args=args)
     node1 = Node1()
-    rclpy.spin(node1)
+    node2 = Node2()
+
+    executor = SingleThreadedExecutor()
+    executor.add_node(node1)
+    executor.add_node(node2)
+    executor.spin()
+
+    # rclpy.spin(node1)
     rclpy.shutdown()
 
 
